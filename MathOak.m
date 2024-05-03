d4862f52b5b088f5af15a9a47086bcb5d470fec3
KDr2 <zhuo.dev@gmail.com>
2024-05-04 07:37:34 +0800

    add function MOSquareQ

--- MathOak.m
+++ MathOak.m
@@ -1,10 +1,12 @@
 BeginPackage["MathOka`"]
 
 (*** Documents ***)
+
 MOVersion::usage = "Version of MathOak."
 
 MOFromDigits::usage = "Like FromDigits, but take a String as its argument and treat the String as List of digits and symbols."
 
+MOSquareQ::usage = "Test if an integer is a square number."
 
 (*** Implementations ***)
 
@@ -13,6 +15,8 @@ Begin["`Private`"]
 MOVersion = "0.0.1"
 
 MOFromDigits[s_String, b_Integer:10] := FromDigits[ToExpression /@ StringSplit[s, ""], b] // Simplify
+
+MOSquareQ[x_] := Element[Sqrt[x], Integers]
 
 End[]
 
